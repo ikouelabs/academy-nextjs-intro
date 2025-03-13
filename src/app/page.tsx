894a1ebf4e77cfc061e21d22f92e2622dce92712
IKOUE <code@ikoue.com>
2025-03-13 14:51:47 +0000

    Counter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+import { useState } from "react";
 
 export default function Home() {
   return (
@@ -7,6 +9,21 @@ export default function Home() {
         the computer<br />
         to life
       </h1>
+
+      <Counter />
     </div>
   );
 }
+
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="pt-10">
+      <button className="mr-3 bg-gray-200 px-4 py-2 rounded-md" onClick={() => setCount(count + 1)}>+</button>
+      <button className="bg-gray-200 px-4 py-2 rounded-md" onClick={() => setCount(count - 1)}>-</button>
+      <div className="mt-4 text-4xl font-semibold">{count}</div>
+    </div>
+  )
+};
